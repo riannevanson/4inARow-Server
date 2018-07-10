c232cb1f49110d021eaef717d93060152e15a0e1
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-07-10 13:57:50 +0200

    6 in a row

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,132 +1,133 @@
-import { 
-  JsonController, Authorized, CurrentUser, Post, Param, BadRequestError, HttpCode, NotFoundError, ForbiddenError, Get, 
-  Body, Patch 
-} from 'routing-controllers'
-import User from '../users/entity'
-import { Game, Player, Board } from './entities'
-import {IsBoard, isValidTransition, calculateWinner, finished} from './logic'
-import { Validate } from 'class-validator'
-import {io} from '../index'
+import {
+  JsonController,
+  Authorized,
+  CurrentUser,
+  Post,
+  Param,
+  BadRequestError,
+  HttpCode,
+  NotFoundError,
+  ForbiddenError,
+  Get,
+  Body,
+  Patch
+} from "routing-controllers";
+import User from "../users/entity";
+import { Game, Player, Board } from "./entities";
+import { IsBoard, isValidTransition, calculateWinner, finished } from "./logic";
+import { Validate } from "class-validator";
+import { io } from "../index";
 
 class GameUpdate {
-
   @Validate(IsBoard, {
-    message: 'Not a valid board'
+    message: "Not a valid board"
   })
-  board: Board
+  board: Board;
 }
 
 @JsonController()
 export default class GameController {
-
   @Authorized()
-  @Post('/games')
+  @Post("/games")
   @HttpCode(201)
-  async createGame(
-    @CurrentUser() user: User
-  ) {
-    const entity = await Game.create().save()
+  async createGame(@CurrentUser() user: User) {
+    const entity = await Game.create().save();
 
     await Player.create({
-      game: entity, 
+      game: entity,
       user,
-      symbol: 'x'
-    }).save()
+      symbol: "x"
+    }).save();
 
-    const game = await Game.findOneById(entity.id)
+    const game = await Game.findOneById(entity.id);
 
-    io.emit('action', {
-      type: 'ADD_GAME',
+    io.emit("action", {
+      type: "ADD_GAME",
       payload: game
-    })
+    });
 
-    return game
+    return game;
   }
 
   @Authorized()
-  @Post('/games/:id([0-9]+)/players')
+  @Post("/games/:id([0-9]+)/players")
   @HttpCode(201)
-  async joinGame(
-    @CurrentUser() user: User,
-    @Param('id') gameId: number
-  ) {
-    const game = await Game.findOneById(gameId)
-    if (!game) throw new BadRequestError(`Game does not exist`)
-    if (game.status !== 'pending') throw new BadRequestError(`Game is already started`)
+  async joinGame(@CurrentUser() user: User, @Param("id") gameId: number) {
+    const game = await Game.findOneById(gameId);
+    if (!game) throw new BadRequestError(`Game does not exist`);
+    if (game.status !== "pending")
+      throw new BadRequestError(`Game is already started`);
 
-    game.status = 'started'
-    await game.save()
+    game.status = "started";
+    await game.save();
 
     const player = await Player.create({
-      game, 
+      game,
       user,
-      symbol: 'o'
-    }).save()
+      symbol: "o"
+    }).save();
 
-    io.emit('action', {
-      type: 'UPDATE_GAME',
+    io.emit("action", {
+      type: "UPDATE_GAME",
       payload: await Game.findOneById(game.id)
-    })
+    });
 
-    return player
+    return player;
   }
 
   @Authorized()
   // the reason that we're using patch here is because this request is not idempotent
   // http://restcookbook.com/HTTP%20Methods/idempotency/
   // try to fire the same requests twice, see what happens
-  @Patch('/games/:id([0-9]+)')
+  @Patch("/games/:id([0-9]+)")
   async updateGame(
     @CurrentUser() user: User,
-    @Param('id') gameId: number,
+    @Param("id") gameId: number,
     @Body() update: GameUpdate
   ) {
-    const game = await Game.findOneById(gameId)
-    if (!game) throw new NotFoundError(`Game does not exist`)
+    const game = await Game.findOneById(gameId);
+    if (!game) throw new NotFoundError(`Game does not exist`);
 
-    const player = await Player.findOne({ user, game })
+    const player = await Player.findOne({ user, game });
 
-    if (!player) throw new ForbiddenError(`You are not part of this game`)
-    if (game.status !== 'started') throw new BadRequestError(`The game is not started yet`)
-    if (player.symbol !== game.turn) throw new BadRequestError(`It's not your turn`)
+    if (!player) throw new ForbiddenError(`You are not part of this game`);
+    if (game.status !== "started")
+      throw new BadRequestError(`The game is not started yet`);
+    if (player.symbol !== game.turn)
+      throw new BadRequestError(`It's not your turn`);
     if (!isValidTransition(player.symbol, game.board, update.board)) {
-      throw new BadRequestError(`Invalid move`)
-    }    
+      throw new BadRequestError(`Invalid move`);
+    }
 
-    const winner = calculateWinner(update.board)
+    const winner = calculateWinner(update.board);
     if (winner) {
-      game.winner = winner
-      game.status = 'finished'
+      game.winner = winner;
+      game.status = "finished";
+    } else if (finished(update.board)) {
+      game.status = "finished";
+    } else {
+      game.turn = player.symbol === "x" ? "o" : "x";
     }
-    else if (finished(update.board)) {
-      game.status = 'finished'
-    }
-    else {
-      game.turn = player.symbol === 'x' ? 'o' : 'x'
-    }
-    game.board = update.board
-    await game.save()
-    
-    io.emit('action', {
-      type: 'UPDATE_GAME',
+    game.board = update.board;
+    await game.save();
+
+    io.emit("action", {
+      type: "UPDATE_GAME",
       payload: game
-    })
+    });
 
-    return game
+    return game;
   }
 
   @Authorized()
-  @Get('/games/:id([0-9]+)')
-  getGame(
-    @Param('id') id: number
-  ) {
-    return Game.findOneById(id)
+  @Get("/games/:id([0-9]+)")
+  getGame(@Param("id") id: number) {
+    return Game.findOneById(id);
   }
 
   @Authorized()
-  @Get('/games')
+  @Get("/games")
   getGames() {
-    return Game.find()
+    return Game.find();
   }
 }
-
--- src/games/logic.ts
+++ src/games/logic.ts
@@ -1,34 +1,43 @@
-import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
-import { Board, Symbol, Row } from './entities'
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface
+} from "class-validator";
+import { Board, Symbol, Row } from "./entities";
 
 @ValidatorConstraint()
 export class IsBoard implements ValidatorConstraintInterface {
-
   validate(board: Board) {
-    const symbols = [ 'x', 'o', null ]
-    return board.length === 3 &&
-      board.every(row =>
-        row.length === 3 &&
-        row.every(symbol => symbols.includes(symbol))
+    const symbols = ["x", "o", null];
+    return (
+      board.length === 7 &&
+      board.every(
+        row => row.length === 7 && row.every(symbol => symbols.includes(symbol))
       )
+    );
   }
 }
 
-export const isValidTransition = (playerSymbol: Symbol, from: Board, to: Board) => {
+export const isValidTransition = (
+  playerSymbol: Symbol,
+  from: Board,
+  to: Board
+) => {
   const changes = from
-    .map(
-      (row, rowIndex) => row.map((symbol, columnIndex) => ({
-        from: symbol, 
+    .map((row, rowIndex) =>
+      row.map((symbol, columnIndex) => ({
+        from: symbol,
         to: to[rowIndex][columnIndex]
       }))
     )
-    .reduce((a,b) => a.concat(b))
-    .filter(change => change.from !== change.to)
+    .reduce((a, b) => a.concat(b))
+    .filter(change => change.from !== change.to);
 
-  return changes.length === 1 && 
-    changes[0].to === playerSymbol && 
+  return (
+    changes.length === 1 &&
+    changes[0].to === playerSymbol &&
     changes[0].from === null
-}
+  );
+};
 
 export const calculateWinner = (board: Board): Symbol | null =>
   board
@@ -36,18 +45,14 @@ export const calculateWinner = (board: Board): Symbol | null =>
       // vertical winner
       [0, 1, 2].map(n => board.map(row => row[n])) as Row[]
     )
-    .concat(
-      [
-        // diagonal winner ltr
-        [0, 1, 2].map(n => board[n][n]),
-        // diagonal winner rtl
-        [0, 1, 2].map(n => board[2-n][n])
-      ] as Row[]
-    )
+    .concat([
+      // diagonal winner ltr
+      [0, 1, 2].map(n => board[n][n]),
+      // diagonal winner rtl
+      [0, 1, 2].map(n => board[2 - n][n])
+    ] as Row[])
     .filter(row => row[0] && row.every(symbol => symbol === row[0]))
-    .map(row => row[0])[0] || null
+    .map(row => row[0])[0] || null;
 
 export const finished = (board: Board): boolean =>
-  board
-    .reduce((a,b) => a.concat(b) as Row)
-    .every(symbol => symbol !== null)
+  board.reduce((a, b) => a.concat(b) as Row).every(symbol => symbol !== null);
